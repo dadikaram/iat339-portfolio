ba9552028a21138814fc95ba997bf1033ca7320d
Dinuki <dadikara@sfu.ca>
2020-11-12 10:26:08 -0800

    Added project artifacts

--- js/logic.js
+++ js/logic.js
@@ -11,7 +11,30 @@ function switchImage(string){
     $('.gallery').attr('src','img/filler-200-blue.png');
     console.log("img3");
   }
+}
 
+function artDirectionSwitch(string){
+  if(string=="one"){
+    $('.gallery').attr('src','img/343_sk4.png');
+    console.log("img1");
+  } else if(string=="two"){
+    $('.gallery').attr('src','img/343_sk5.png');
+    console.log("img2");
+  }
+  // else if(string=="three"){
+  //   $('.gallery').attr('src','img/filler-200-blue.png');
+  //   console.log("img3");
+  // }
+}
+
+function ntIdeationSwitch(string){
+  if(string=="one"){
+    $('.gallery').attr('src','img/nt-mindmap1.png');
+    console.log("img1");
+  } else if(string=="two"){
+    $('.gallery').attr('src','img/nt-mindmap2.png');
+    console.log("img2");
+  }
 }
 
 
